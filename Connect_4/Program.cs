1ac64d0293c5fb788fd1321b447e672c4d89076d
Jansen <ajagcaoili4@gmail.com>
2025-04-16 18:56:43 -0600

    Combined the board and and player class for players to be able to play

--- Connect_4/Program.cs
+++ Connect_4/Program.cs
@@ -40,57 +40,112 @@ namespace Connect_4
         // Here it will be implemented the whole game.
         class NewGame
         {
-
-        }
-
-        // Game Board output
-        // Needs to have 7 columns and 7 rows.
-        class Board
-        {
-            //I added these but feel free to change them - Derek
-            public char [,] gameBoard = new char [7, 7];
-
-            //Don't know if we need this, but will be used to initialize each element of the array with _ to show that they are empty.
-            public void populateBoard()
+            public void Start()
             {
-                // 7 =  amount of game board columns
-                for (int i = 0; i < 7; i++)
-                {
-                    for (int j = 0; j < 7; j++)
-                    {
-                        gameBoard[i, j] = '_';
-                        Console.Write(gameBoard[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
+                Board board = new Board();
+                //Specifically put '' for char instead of ""
+                Player player1 = new RealPlayer('X');
+                Player player2 = new RealPlayer('O');
+                Player currentPlayer = player1;
 
-            public void placeUserTurnOnBoard (char userSymbol)
-            {
-                // 7 =  amount of game board columns
-                for (int i = 0; i < 7; i++)
+                // Initialize and show empty board
+                board.populateBoard();
+                board.printBoard();
+
+                while (true)
                 {
-                    //counting from bottom to top instead of top to bottom
-                    for (int j = 6; j >= 0; j--)
+                    // Get the move from current player
+                    int column = currentPlayer.GetMove();
+
+                    // Try placing the piece
+                    bool success = board.userTurnOnTheBoard(column, currentPlayer.Symbol);
+
+                    if (success)
                     {
-                        //if that index is "empty", replace with user symbol
-                        if (gameBoard[i, j] == '_')
+                        board.printBoard();
+                        if (currentPlayer == player1)
                         {
-                            gameBoard[i, j] = userSymbol;
+                            currentPlayer = player2;
+                        }
+                        else
+                        {
+                            currentPlayer = player1;
                         }
                     }
                 }
             }
         }
 
+
+        // Game Board output
+        // Needs to have 7 columns and 7 rows.
+        class Board
+        {
+            // I added these but feel free to change them - Derek
+            public char[,] gameBoard = new char[7, 7];
+
+            // Don't know if we need this, but will be used to initialize each element of the array with _ to show that they are empty.
+            public void populateBoard()
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        gameBoard[i, j] = '_'; // '_' represents an empty cell
+                    }
+                }
+            }
+
+            //Another method to print the current gameboard on the screen
+            public void printBoard()
+            {
+                Console.WriteLine();
+
+                for (int i = 0; i < 7; i++)
+                {
+
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write(gameBoard[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("0 1 2 3 4 5 6");
+                Console.WriteLine();
+            }
+
+            public bool userTurnOnTheBoard(int column, char userSymbol)
+            {
+                if (column < 0 || column > 6)
+                {
+                    Console.WriteLine("Please choose between 0-6!");
+                    return false;
+                }
+
+                for (int row = 6; row >= 0; row--)
+                {
+                    if (gameBoard[row, column] == '_')
+                    {
+                        gameBoard[row, column] = userSymbol;
+                        return true;
+                    }
+                }
+
+                // If the loop completes, the column is full
+                Console.WriteLine("Column is already full! Please use other available columns.");
+                return false; // Unsuccessful placement
+            }
+        }
+
+
         // Static void that shows instructions
         static void Instructions()
         {
-            Console.WriteLine("About Connect 4");
+            Console.WriteLine("\n\nAbout Connect 4");
             Console.WriteLine("Connect Four (also known as Connect 4, Four Up, Plot Four, Find Four, Captain's Mistress, Four in a Row, Drop Four, and in the Soviet Union, Gravitrips) ");
             Console.WriteLine("is a game in which the players choose a color and then take turns dropping colored tokens into a six-row, seven-column vertically suspended grid. ");
             Console.WriteLine("The pieces fall straight down, occupying the lowest available space within the column. ");
-            Console.WriteLine("The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical,\n or diagonal line of four of one's own tokens. (Wikipedia).");
+            Console.WriteLine("The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens. (Wikipedia).");
             Console.WriteLine("\nProgram was made by Arnold Jansen Agcaoili and Derek Carlos");
         }
 
@@ -114,7 +169,8 @@ namespace Connect_4
                 string choice = Console.ReadLine();
                 if (choice == "1")
                 {
-                    
+                    NewGame game = new NewGame();
+                    game.Start();
                     return;
 
                 }
